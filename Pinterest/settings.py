4240a4866ee807e6c0767c31c830d45ec98c08c4
DevOpes <65594389+Oprahjosh@users.noreply.github.com>
2021-07-16 19:32:03 +0100

    Update settings.py

--- Pinterest/settings.py
+++ Pinterest/settings.py
@@ -123,10 +123,11 @@ USE_TZ = True
 
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
