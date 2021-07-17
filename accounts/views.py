fb6b45f427d62684744ecc0abbf242ac1d129627
DevOpes <65594389+Oprahjosh@users.noreply.github.com>
2021-07-17 04:22:57 +0100

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,24 +1,23 @@
 from django.contrib.auth import logout , login
 from django.shortcuts import render,redirect
 from .forms import RegisterForm,LoginForm
-from django.http import request,HttpRequest
-
+from django.http import request
 
 # Create your views here.
 
 def index(request):
     return render(request, 'accounts/index.html', )
 
-def register(response):
-    if response.method == 'POST':
-        form = RegisterForm(response.POST)
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
         return redirect('/dashboard')
     else:
         form = RegisterForm
-    return render ( response , 'accounts/signup.html' , {'form': form} )
+    return render ( request , 'accounts/signup.html' , {'form': form} )
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html', )
